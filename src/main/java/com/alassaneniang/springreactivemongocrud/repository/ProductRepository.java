88d5d90091c4939ec7e55d978576153d5330513e
Alassane NIANG <adniang75@gmail.com>
2022-06-30 15:30:55 +0200

    Implemented Product Repository

---
+++ src/main/java/com/alassaneniang/springreactivemongocrud/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.alassaneniang.springreactivemongocrud.repository;
+
+import com.alassaneniang.springreactivemongocrud.entity.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+
+}
